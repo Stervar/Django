c4d73ace1ea0ea8fa5cd89ef63237f3604b669ad
stervar <gaberkonvadim8@gmail.com>
2024-09-27 21:17:58 +0300

    vg

--- Sbornik6/DER/views.py
+++ Sbornik6/DER/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
 
+
+
+
+
+
 def index(request):
-    return HttpResponse("Hello DER")
+    return render(request, "index.html")
