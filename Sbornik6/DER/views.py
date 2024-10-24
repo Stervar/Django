da9f9a91f58894435abc650f9c66182850f2cc76
Stervar <150718158+Stervar@users.noreply.github.com>
2024-10-24 22:20:36 +0300

    vg

--- Sbornik6/DER/views.py
+++ Sbornik6/DER/views.py
@@ -3,11 +3,3 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.shortcuts import render
-from datetime import datetime
-
-def current_datetime(request):
-    now = datetime.now()
-    return render(request, 'current_datetime.html', {'current_datetime': now})
-
